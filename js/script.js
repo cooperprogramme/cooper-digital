e7fd37776e707ac54744695e279aa4c08992b3ae
Cooper <70655034+cooperprogramme@users.noreply.github.com>
2023-09-12 11:28:48 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-$(document).ready(function() {
-  $(document).on('mousemove', function(e) {
-    $('#cursor').css({
-      left: e.pageX,
-      top: e.pageY
-    });
-  })
-});
+document.body.onmousemove = function(e) {
+  document.documentElement.style.setProperty (
+    '--x', (
+      e.clientX+window.scrollX
+    )
+    + 'px'
+  );
+  document.documentElement.style.setProperty (
+    '--y', (
+      e.clientY+window.scrollY
+    ) 
+    + 'px'
+  );
+}
 
 var acc = document.getElementsByClassName("accordion");
 var i;
